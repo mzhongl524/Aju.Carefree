f20161513172e80f6c9fb33aba236b897befb86b
Prince <1022560838@qq.com>
2019-05-08 16:25:51 +0800

    新建Web后台框架并使用AdminLTE构建首页

---
+++ Aju.Carefree.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aju.Carefree.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
